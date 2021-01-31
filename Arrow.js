1afa7a346c847a4a84f96f2ed2ae0c3099bb8cc6
RianIslam <rianislamdr@gmail.com>
2021-02-01 02:20:27 +0600

    arrow

--- Arrow.js
+++ Arrow.js
@@ -6,7 +6,17 @@
 // const result = doubleIt(4);
 // console.log(result);
 
-const add =(x,y) => x+y;
+// const add =(x,y) => x+y;
 
-const result = add(34,55);
-console.log(result);
+// const result = add(34,55);
+// console.log(result);
+
+const doMath =(x,y) =>{
+    const sum = x+y;
+    const diff = x-y;
+    const  result = sum * diff;
+    return result;
+}
+
+const all = doMath(2,3);
+console.log(all);
