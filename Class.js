69e8723fac5f44df03dbc787ba8e603460c50203
RianIslam <rianislamdr@gmail.com>
2021-02-01 02:51:35 +0600

    class and constructor

--- Class.js
+++ Class.js
@@ -1,9 +1,11 @@
 class  Student{
     constructor(sId,sName){
-
+        this.id = sId;
+        this.name = sName;
+        this.school = "pabna zilla school"
     }
 }
 
-const student1 = new Student(12,"shovo");
+const student1 = new Student(12,"rian");
 const student2 = new Student(13, "karim");
 console.log(student1,student2)
