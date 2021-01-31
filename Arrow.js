2b4a2a71b69134714f4f6d7bde270ca69253ee7a
RianIslam <rianislamdr@gmail.com>
2021-02-01 02:13:26 +0600

    arrow

--- Arrow.js
+++ Arrow.js
@@ -1,7 +1,12 @@
-const doubleIt = function myFun(num){
-    return num *2;
+// const doubleIt = function myFun(num){
+//     return num *2;
 
-}
+// }
 
-const result = doubleIt(4);
+// const result = doubleIt(4);
+// console.log(result);
+
+const add =(x,y) => x+y;
+
+const result = add(34,55);
 console.log(result);
