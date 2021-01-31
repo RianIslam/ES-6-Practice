90cd0014051e3cca5ba8d9fac32efe0a3f6e2ed6
RianIslam <rianislamdr@gmail.com>
2021-02-01 01:12:58 +0600

    default

--- Template.js
+++ Template.js
@@ -2,4 +2,10 @@ const firstName = "justin";
 const lastName = "beiber";
 const fullName = firstName + " " + lastName;
 const fullName2 = `${firstName} ${lastName} is a boy`;
+
+const multiline = `i love u
+i miss you 
+no i don't need you
+baily road a dorkar nai`
+
 console.log(fullName2);
