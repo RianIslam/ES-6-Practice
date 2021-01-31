d31669d476314880439eb9badb52017b362f35a6
RianIslam <rianislamdr@gmail.com>
2021-02-01 01:08:17 +0600

    default

---
+++ Template.js
@@ -0,0 +1,5 @@
+const firstName = "justin";
+const lastName = "beiber";
+const fullName = firstName + " " + lastName;
+const fullName2 = `${firstName} ${lastName} is a boy`;
+console.log(fullName2);
